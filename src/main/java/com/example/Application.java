84d48112f6b881a78a65af526d6df4ebb9a641f2
Lyubomir Galabov <lvgalabov@gmail.com>
2024-03-06 01:17:00 +0200

    fix: rework gradle properties

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,8 +1,22 @@
 package com.example;
 
+import com.example.config.Environment;
+import io.micronaut.context.ApplicationContextBuilder;
+import io.micronaut.context.ApplicationContextConfigurer;
+import io.micronaut.context.annotation.ContextConfigurer;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
+
+    @ContextConfigurer
+    @SuppressWarnings("unused")
+    public static class DefaultEnvironmentConfigurer implements ApplicationContextConfigurer {
+        @Override
+        public void configure(@NonNull ApplicationContextBuilder builder) {
+            builder.defaultEnvironments(Environment.API_MODE);
+        }
+    }
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
